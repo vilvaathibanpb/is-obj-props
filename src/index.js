cafa79f58f14394442783b79510ff9eb9ba1111c
Vilva Athipan <vilva.athipan@Vilva-MacBook-Pro.local>
2018-07-19 19:48:20 +0530

    Added utility

---
+++ src/index.js
@@ -0,0 +1,19 @@
+export default (isObjProps = (obj, propsArray) => {
+  if (!obj) {
+    return null;
+  }
+  let objCopy = Object.assign({}, obj);
+
+  for (let i = 0; i < propsArray.length; i++) {
+    if (
+      typeof objCopy[propsArray[i]] !== "undefined" &&
+      objCopy[propsArray[i]] !== null
+    ) {
+      objCopy = objCopy[propsArray[i]];
+    } else {
+      return null;
+    }
+  }
+
+  return objCopy;
+});
